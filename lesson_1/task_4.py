846035c1a2ffe10536ae99fa2b85de46a652ddb3
Leo Kazantsev <leokazantsev19@mail.ru>
2022-06-27 02:48:02 +0500

    added the new improves

--- lesson_1/task_4.py
+++ lesson_1/task_4.py
@@ -7,32 +7,26 @@ def compress_improve(data, selectors):
             list_of_values.append(d)
         else:
             list_of_values.append("-")      
-    return list_of_values
+    return ''.join(list_of_values)
 
 
 def bananas(s) -> set:
     result = set()
-    count_of_opt = list(it.product([1, 0], repeat = len(s)))
-    count_of_itters = 0
-    str_ = ''
+    count_of_opt = it.product([1, 0], repeat = len(s))
     for value in count_of_opt:
-        value_ = ''.join(compress_improve(s, value))
-        
-        for i in value_:
-            if i != "-":
-                str_ += i
-        if str_ == "banana":
+          
+        value_ = compress_improve(s, value)
+        if value_.replace("-", "") == "banana":
             result.add(value_)
-            count_of_itters += 1
-            
-        str_ = ''
-     
     return result
+
 
 assert bananas("banann") == set()
 assert bananas("banana") == {"banana"}
+
 assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
                      "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
                      "-ban--ana", "b-anana--"}
 assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
 assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
+
--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,41 +1,24 @@
-import itertools as it
-
 def count_find_num(primesL, limit):
-    list_of_degree = list()
-    list_of_numbers = list()
-    #if limit <= 1000:
-    for i in range(1, 11):
-        list_of_degree.append(i)
-        
-    count_of_opt = it.product(list_of_degree, repeat=len(primesL))
-    temp = 1
-    for value in count_of_opt:
-        for i in range(len(value)):
-            temp *= (primesL[i] ** value[i])
-        list_of_numbers.append(temp)
-        temp = 1
     
-    list_of_numbers = sorted(list_of_numbers)
-    
-    for value in list_of_numbers:
-        if value > limit:
-            list_of_numbers = list_of_numbers[:list_of_numbers.index(value)]
-            break
-    if list_of_numbers:
-        pass
-    else:
-        return []   
-    return [len(list_of_numbers), list_of_numbers[-1]]
+    primesL = sorted(primesL)
+    start_value = 1
+    for value in primesL:
+        start_value *= value
+    if start_value > limit:
+        return []
+    general_numbers = [start_value]
+    for i in primesL:
+        for value in general_numbers:
+            number = value * i
+            if (number <= limit) and (number not in general_numbers):
+                general_numbers.append(number)
+                number *= i
+
+    return [len(general_numbers), max(general_numbers)]
    
-    
-primesL = [2, 5, 7]
-limit = 500
-assert count_find_num(primesL, limit) == [5, 490]
-
-
-primesL = [2, 3]
-limit = 200
-assert count_find_num(primesL, limit) == [13, 192]
+primesL = [2, 31, 67, 491]
+limit = 951261292747
+assert count_find_num(primesL, limit) ==  [100, 613440425482]
 
 primesL = [2, 5]
 limit = 200
@@ -52,3 +35,4 @@ assert count_find_num(primesL, limit) == [19, 960]
 primesL = [2, 3, 47]
 limit = 200
 assert count_find_num(primesL, limit) == []
+
