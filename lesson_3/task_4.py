4ecc8b960ac2eff93533dc8dac6f7d9f8fca3f44
Leo Kazantsev <leokazantsev19@mail.ru>
2022-07-10 12:56:02 +0500

    added some files

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -1,16 +1,16 @@
 import time
-def dec_for_repeat_of_func(*args):
+def dec_for_repeat_of_func(call_count, start_sleep_time, factor, border_sleep_time):
     def for_repeat_func(func):
         def wrapper():
-            t = args[1]
+            t = start_sleep_time
             counter = 1
-            temp = args[1]
-            while counter <= args[0]:
+            temp = start_sleep_time
+            while counter <= call_count:
                 print(f"Запуск номер {counter}. Ожидание: {t} секунд.") 
                 time.sleep(t)
                 print(f"Результат декорируемой функций = {func()}.")
                 counter += 1
-                t = temp * 2 ** args[2]
+                t = temp * 2 ** factor
                 temp = t
                   
         return wrapper
