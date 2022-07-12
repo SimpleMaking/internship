e8c41ccce5e15c3b22e7a9135d76c966ac0bb471
Leo Kazantsev <leokazantsev19@mail.ru>
2022-07-12 15:14:14 +0500

    modified file media.py

--- lesson_3/task_5/solid_code/media.py
+++ lesson_3/task_5/solid_code/media.py
@@ -1,4 +1,6 @@
 class Media():
-    def create_news(self, place, name):
+    def create_news(self, place, name, coordinates:list = ['Mars', 'Upiter', 'Earth']):
         place_name = getattr(place, 'name', 'place')
         print(f'{name} saved the {place_name}!')
+        for value in coordinates:
+            print(f'{value} get our info!')
