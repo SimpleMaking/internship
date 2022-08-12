7dd1ca043da63a16e75366163fb316b0b3772561
Leo Kazantsev <leokazantsev19@mail.ru>
2022-08-12 14:05:43 +0500

    edited task_1

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -1,27 +1,10 @@
-
-
-
 def counter_of_symbols(str: str):
     str = str.lower()
     dictionary = dict()
-    counter = 0
-    last_symb = None
-    first = True
-    for symbol in (list(str) + [None]):
-        if first:
-            dictionary[symbol] = 1
-            first = False
-            last_symb = symbol
-        
-        if last_symb and symbol != last_symb:
-            
-            dictionary[last_symb] = counter
-            counter = 1
-            last_symb = symbol
-
-        else:  
-          counter += 1  
-    
+  
+    for symbol in str:
+        dictionary[symbol] = dictionary.get(symbol, 0) + 1
+     
     #sorting
     sorted_dictionary = dict()
     sorted_keys = sorted(dictionary, key=dictionary.get)  
@@ -29,6 +12,6 @@ def counter_of_symbols(str: str):
     for key in sorted_keys:
         sorted_dictionary[key] = dictionary[key]
  
-    return(list(sorted_dictionary.keys())[-1], sorted_dictionary[list(sorted_dictionary.keys())[-1]])
+    return (list(sorted_dictionary.keys())[-1], sorted_dictionary[list(sorted_dictionary.keys())[-1]])
 
-print(counter_of_symbols('aaaAAAbc'))
+print(counter_of_symbols('CsCCCccccaaaaAAAbc'))
