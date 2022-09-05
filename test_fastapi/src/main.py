12c604e95e0a07c5fa2d3f9bb07fe839817f1433
Leo Kazantsev <leokazantsev19@mail.ru>
2022-09-05 18:26:56 +0300

    modified main.py

--- test_fastapi/src/main.py
+++ test_fastapi/src/main.py
@@ -1,3 +1,4 @@
+'''
 import uvicorn
 from fastapi import FastAPI
 import routes
@@ -14,12 +15,65 @@ app = FastAPI(
     # Адрес документации в формате OpenAPI
     openapi_url="/api/openapi.json",
 )
-
+'''
 
 # Подключаем роутеры к серверу
-app.include_router(router=routes.router)
+#app.include_router(router=routes.router)
+from urllib.request import Request
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+import uvicorn
 
 
+app = FastAPI()
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>Test WebSocket</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <textarea id="messageText" autocomplete="off "name="post" cols="40" rows="3"></textarea>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"New Message: {data}")
+
+      
 if __name__ == "__main__":
     # Приложение может запускаться командой
     # `uvicorn main:app --host 127.0.0.1 --port 8000`
